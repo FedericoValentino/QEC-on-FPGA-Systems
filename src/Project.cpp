2ce24ed3f0004ba5af5fb19d9f825cbc14d31f04
FedericoValentino <10679810@polimi.it>
2023-06-02 15:59:32 +0200

    Synthesis should be possible

--- src/Project.cpp
+++ src/Project.cpp
@@ -6,7 +6,23 @@ Decoder decoderUF;
 
 void test(int root1, int root2)
 {
-#pragma HLS ARRAY_PARTITION variable=decoderUF.border_vertices.map.array->v2.array type=cyclic factor=16
+#pragma HLS ARRAY_PARTITION variable=decoderLUT.map type=complete
+	//rootManager
+#pragma HLS ARRAY_PARTITION variable=decoderUF.mngr.roots.array type=cyclic factor=16
+#pragma HLS ARRAY_PARTITION variable=decoderUF.mngr.oddRoots.array type=cyclic factor=16
+#pragma HLS ARRAY_PARTITION variable=decoderUF.mngr.sizes.map.array type=cyclic factor=128
+#pragma HLS ARRAY_PARTITION variable=decoderUF.mngr.parity.map.array type=cyclic factor=128
+	//Decoder
+#pragma HLS ARRAY_PARTITION variable=decoderUF.connection_counts.array type=cyclic factor=16
+#pragma HLS ARRAY_PARTITION variable=decoderUF.support.array type=cyclic factor=16
+#pragma HLS ARRAY_PARTITION variable=decoderUF.root_of_vertex.array type=cyclic factor=16
+#pragma HLS ARRAY_PARTITION variable=decoderUF.fuseList.array type=cyclic factor=16
+#pragma HLS ARRAY_PARTITION variable=decoderUF.border_vertices.map.array->v2 type=cyclic factor=16
+#pragma HLS ARRAY_PARTITION variable=decoderUF.peeling_edges.array type=cyclic factor=16
+
+
+	decoderUF.mngr.merge(root1, root2);
+	decoderUF.mergeBoundary(root1, root2);
 	decoderUF.border_vertices.erase(root2);
 }
 
@@ -36,6 +52,7 @@ void decoderTop(int syndrome[SYN_LEN], ap_uint<CORR_LEN>* correction, bool inser
 		if(*correction == 0)
 		{
 			*correction = decoderUF.decode(syndrome);
+			decoderUF.clear();
 		}
 	}
 
