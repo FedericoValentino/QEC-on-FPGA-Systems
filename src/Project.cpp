278f8118ffc9cc2ad66b31463952f75fe29afab4
FedericoValentino <10679810@polimi.it>
2023-06-10 15:28:56 +0200

    FIX

--- src/Project.cpp
+++ src/Project.cpp
@@ -3,7 +3,7 @@
 void decoderTop(int syndrome[SYN_LEN], ap_uint<CORR_LEN>* correction, bool insert)
 {
 	static HashMap decoderLUT;
-	//static Decoder decoderUF;
+	static Decoder decoderUF;
 	//axi
 #pragma HLS INTERFACE m_axi port=syndrome offset=slave bundle=gmem0 depth=128
 #pragma HLS INTERFACE m_axi port=correction offset=slave bundle=gmem1 depth=256
@@ -13,7 +13,7 @@ void decoderTop(int syndrome[SYN_LEN], ap_uint<CORR_LEN>* correction, bool inser
 #pragma HLS INTERFACE s_axilite port=insert bundle=control
 	//hashmap
 #pragma HLS ARRAY_PARTITION variable=decoderLUT.map type=complete
-	/*//rootManager
+	//rootManager
 #pragma HLS ARRAY_PARTITION variable=decoderUF.mngr.roots.array type=cyclic factor=16
 #pragma HLS ARRAY_PARTITION variable=decoderUF.mngr.oddRoots.array type=cyclic factor=16
 #pragma HLS ARRAY_PARTITION variable=decoderUF.mngr.sizes.map type=cyclic factor=128
@@ -24,7 +24,7 @@ void decoderTop(int syndrome[SYN_LEN], ap_uint<CORR_LEN>* correction, bool inser
 #pragma HLS ARRAY_PARTITION variable=decoderUF.root_of_vertex type=complete
 #pragma HLS ARRAY_PARTITION variable=decoderUF.border_vertices.map->array type=cyclic factor=16
 #pragma HLS STREAM variable=decoderUF.fuseList depth=64
-#pragma HLS STREAM variable=decoderUF.peeling_edges depth=64*/
+#pragma HLS STREAM variable=decoderUF.peeling_edges depth=64
 	ap_uint<CORR_LEN> tmp;
 	if(insert)
 	{
@@ -33,14 +33,12 @@ void decoderTop(int syndrome[SYN_LEN], ap_uint<CORR_LEN>* correction, bool inser
 	else
 	{
 		tmp = decoderLUT.retrieve(syndrome);
-		/*
 		if(tmp == 0)
 		{
 			decoderUF.clear();
 			tmp = decoderUF.decode(syndrome);
 		}
 		*correction = tmp;
-		*correction */
 	}
 
 
--- src/Project.h
+++ src/Project.h
@@ -5,6 +5,8 @@
 #include "LUT/Hashmap.h"
 #include "UnionFind/Decoder.h"
 
+
+
 void decoderTop(int syndrome[SYN_LEN], ap_uint<CORR_LEN>* correction, bool insert);
 
 #endif
