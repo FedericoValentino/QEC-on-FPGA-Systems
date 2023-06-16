34c3f558b6a8d78d1c0302c684ac1da9809b8a94
francesco.scroccarello <francesco.scroccarello@NAGS21>
2023-06-16 10:07:04 +0200

    makefiles

---
+++ makefile_us_alveo.mk
@@ -0,0 +1,118 @@
+############################## Help Section ##############################
+ifneq ($(findstring Makefile, $(MAKEFILE_LIST)), Makefile)
+help:
+	$(ECHO) "Makefile Usage:"
+	$(ECHO) "  make all TARGET=<sw_emu/hw_emu/hw> PLATFORM=<FPGA platform>"
+	$(ECHO) "      Command to generate the design for specified Target and Shell."
+	$(ECHO) ""
+	$(ECHO) "  make run TARGET=<sw_emu/hw_emu/hw> PLATFORM=<FPGA platform>"
+	$(ECHO) "      Command to run application in emulation."
+	$(ECHO) ""
+	$(ECHO) "  make build TARGET=<sw_emu/hw_emu/hw> PLATFORM=<FPGA platform>"
+	$(ECHO) "      Command to build xclbin application."
+	$(ECHO) ""
+	$(ECHO) "  make host"
+	$(ECHO) "      Command to build host application."
+	$(ECHO) ""
+	$(ECHO) "  make clean "
+	$(ECHO) "      Command to remove the generated non-hardware files."
+	$(ECHO) ""
+	$(ECHO) "  make cleanall"
+	$(ECHO) "      Command to remove all the generated files."
+	$(ECHO) ""
+
+endif
+
+############################## Setting up Project Variables ##############################
+TARGET := hw
+VPP_LDFLAGS :=
+include ./utils.mk
+
+TEMP_DIR := ./_x.$(TARGET).$(XSA)
+BUILD_DIR := ./build_dir.$(TARGET).$(XSA)
+
+LINK_OUTPUT := $(BUILD_DIR)/FRATC.link.xclbin
+PACKAGE_OUT = ./package.$(TARGET)
+
+VPP_PFLAGS := 
+CMD_ARGS = $(BUILD_DIR)/FRATC.xclbin
+CXXFLAGS += -I$(XILINX_XRT)/include -I$(XILINX_VIVADO)/include -Wall -O0 -g -std=c++1y
+LDFLAGS += -L$(XILINX_XRT)/lib -pthread -lOpenCL
+
+########################## Checking if PLATFORM in allowlist #######################
+PLATFORM_BLOCKLIST += nodma 
+############################## Setting up Host Variables ##############################
+#Include Required Host Source Files
+HOST_SRCS += hostcode/host.cpp hostcode/host.h 
+# Host compiler global settings
+CXXFLAGS += -fmessage-length=0
+LDFLAGS += -lrt -lstdc++ 
+
+############################## Setting up Kernel Variables ##############################
+# Kernel compiler global settings
+VPP_FLAGS += --save-temps 
+
+
+EXECUTABLE = ./FRATC
+EMCONFIG_DIR = $(TEMP_DIR)
+
+############################## Setting Targets ##############################
+.PHONY: all clean cleanall docs emconfig
+all: check-platform check-device check-vitis $(EXECUTABLE) $(BUILD_DIR)/FRATC.xclbin emconfig
+
+.PHONY: host
+host: $(EXECUTABLE)
+
+.PHONY: build
+build: check-vitis check-device $(BUILD_DIR)/FRATC.xclbin
+
+.PHONY: xclbin
+xclbin: build
+
+############################## Setting Rules for Binary Containers (Building Kernels) ##############################
+$(TEMP_DIR)/FRATC.xo: src/Project.cpp
+	mkdir -p $(TEMP_DIR)
+	v++ -c $(VPP_FLAGS) -t $(TARGET) --platform $(PLATFORM) -k decoderTop --temp_dir $(TEMP_DIR)  -I'$(<D)' -o'$@' '$<'
+
+$(BUILD_DIR)/FRATC.xclbin: $(TEMP_DIR)/FRATC.xo
+	mkdir -p $(BUILD_DIR)
+	v++ -l $(VPP_FLAGS) $(VPP_LDFLAGS) -t $(TARGET) --platform $(PLATFORM) --temp_dir $(TEMP_DIR) -o'$(LINK_OUTPUT)' $(+)
+	v++ -p $(LINK_OUTPUT) $(VPP_FLAGS) -t $(TARGET) --platform $(PLATFORM) --package.out_dir $(PACKAGE_OUT) -o $(BUILD_DIR)/FRATC.xclbin
+
+############################## Setting Rules for Host (Building Host Executable) ##############################
+$(EXECUTABLE): $(HOST_SRCS) | check-xrt
+		g++ -o $@ $^ $(CXXFLAGS) $(LDFLAGS)
+
+emconfig:$(EMCONFIG_DIR)/emconfig.json
+$(EMCONFIG_DIR)/emconfig.json:
+	emconfigutil --platform $(PLATFORM) --od $(EMCONFIG_DIR)
+
+############################## Setting Essential Checks and Running Rules ##############################
+run: all
+ifeq ($(TARGET),$(filter $(TARGET),sw_emu hw_emu))
+	cp -rf $(EMCONFIG_DIR)/emconfig.json .
+	XCL_EMULATION_MODE=$(TARGET) $(EXECUTABLE) $(CMD_ARGS)
+else
+	$(EXECUTABLE) $(CMD_ARGS)
+endif
+
+.PHONY: test
+test: $(EXECUTABLE)
+ifeq ($(TARGET),$(filter $(TARGET),sw_emu hw_emu))
+	XCL_EMULATION_MODE=$(TARGET) $(EXECUTABLE) $(CMD_ARGS)
+else
+	$(EXECUTABLE) $(CMD_ARGS)
+endif
+
+############################## Cleaning Rules ##############################
+# Cleaning stuff
+clean:
+	-$(RMDIR) $(EXECUTABLE) $(XCLBIN)/{*sw_emu*,*hw_emu*} 
+	-$(RMDIR) profile_* TempConfig system_estimate.xtxt *.rpt *.csv 
+	-$(RMDIR) src/*.ll *v++* .Xil emconfig.json dltmp* xmltmp* *.log *.jou *.wcfg *.wdb
+
+cleanall: clean
+	-$(RMDIR) build_dir*
+	-$(RMDIR) package.*
+	-$(RMDIR) _x* *xclbin.run_summary qemu-memory-_* emulation _vimage pl* start_simulation.sh *.xclbin
+
